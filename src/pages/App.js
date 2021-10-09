8c6770eead73d31dcc4a056316454e940ec10373
SciDroid <scidroidgames@gmail.com>
2021-10-09 20:00:25 +0000

    add form

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,18 +1,25 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import Aside from "../partials/Aside";
+import FormUI from "../partials/Form";
 
 function Dash() {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
-    return <div>Loading ...</div>;
+    return (
+      <div className="flex h-screen">
+        <div className="m-auto">
+          <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+        </div>
+      </div>
+    );
   }
-  
+
   return (
     isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+      <div class="flex">
+        <Aside avatar={user.picture} />
+        <FormUI />
       </div>
     )
   );
