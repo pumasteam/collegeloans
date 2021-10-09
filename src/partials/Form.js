030b48f1b60011b2c9188d242d62d607f824e88d
SciDroid <scidroidgames@gmail.com>
2021-10-09 20:15:22 +0000

    add metatags

--- src/partials/Form.js
+++ src/partials/Form.js
@@ -31,7 +31,7 @@ export default function FormUI(props) {
             <Field
               className="m-1 max-w-sm rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
               name="state"
-              placeholder="You state"
+              placeholder="Your state"
             />
             <Field
               className="m-1 max-w-sm rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
